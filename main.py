d8f17717f76186a06b25d1818fca8795a27ad92e
Siddartha Eleti <seleti@g.clemson.edu>
2016-03-07 09:45:07 -0600

    commenting out stock code

--- main.py
+++ main.py
@@ -29,21 +29,21 @@ def places():
     return request.get(url)
 
 
-@app.route('/stock')
-def stock():
-    ticker = request.args.get('text')
-    # Get price, amount change, percent change, day high, day low
-    url = "http://download.finance.yahoo.com/d/quotes.csv?s=" + ticker + "&f=l1c1p2hg"
-    result = requests.get(url).text.split(',')
-    result[2] = result[2].replace('"','')
-    result[4] = result[4].replace('\n','')
-    result_text = ("*" + ticker.upper() + "*" + "\n"
-                    "Price: " + result[0] + "\n"
-                    "Amt Change: " + result[1] + "\n"
-                    "% Change: " + result[2] + "\n"
-                    "Day High: " + result[3] + "\n"
-                    "Day Low: " + result[4])
-    return result_text
+# @app.route('/stock')
+# def stock():
+#     ticker = request.args.get('text')
+#     # Get price, amount change, percent change, day high, day low
+#     url = "http://download.finance.yahoo.com/d/quotes.csv?s=" + ticker + "&f=l1c1p2hg"
+#     result = requests.get(url).text.split(',')
+#     result[2] = result[2].replace('"','')
+#     result[4] = result[4].replace('\n','')
+#     result_text = ("*" + ticker.upper() + "*" + "\n"
+#                     "Price: " + result[0] + "\n"
+#                     "Amt Change: " + result[1] + "\n"
+#                     "% Change: " + result[2] + "\n"
+#                     "Day High: " + result[3] + "\n"
+#                     "Day Low: " + result[4])
+#     return result_text
 
 @app.route("/")
 def hello():
